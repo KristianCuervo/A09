dcd682099f2d9a217a907a69aaedac42b7edd850
Archan Chumsena <a.chumsena@student.tudelft.nl>
2020-11-11 23:20:40 +0700

    variables setup for main calculator

--- WP4.3/GustCalc.py
+++ WP4.3/GustCalc.py
@@ -1,1 +1,21 @@
+from GustTools import *
 
+#Constants
+c = 2.834797715         #mean geometric chord [m]
+S = 78.90625072         #wing surface area [m2]
+C_L_alpha_M0 = 5.2      #base lift slope [1/rad]
+V_C = 250.8078156       #cruise velocity (TAS) [m/s]
+Z_mo =                  #maximum operating altitude [m]
+MTOW = 262631.8937      #maximum take off weight [N]
+W_land_max =            #maximum landing weight [N]
+ZFW_max = 147482.2094   #maximum zero fuel weight (OEW + Pay_max) [N]
+
+g = 9.80665             #gravitational acceleration [m/s2]
+Rho_0 = 1.225           #sea level density [kg/m3]
+p_0 = 101325            #sea level pressure [Pa]
+T_0 = 288.15            #sea level temperature [K]
+gamma = 1.4             #specific heat ratios of air [-]
+
+#Per diagram inputs
+h =                     #considered altitude [m]
+W =                     #weight considered [N]
