834d1461a6d7ba08185faa4c9619160d7b892740
Thymen5225 <74298132+Thymen5225@users.noreply.github.com>
2020-11-30 15:44:16 +0100

    Add files via upload

---
+++ TensileCompressiveStress.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy as sp
+# import inertia stuff and geomtry
+
+from wingbox_properties import *
+
+# import momentforce from NormalShearMomentDiagram
+
+from NormalShearMomentDiagrams import Momentforce, Momentforce1
+
+# calculate sigma function
+
+
+# for loop to make it into an array
+
+
+# make graph
+
+
+# make polynomial formula
