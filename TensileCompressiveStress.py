9ae741d637a5a5a79456ca32ad76c146b9b60a00
A-S1 <74262659+A-S1@users.noreply.github.com>
2020-11-30 16:24:30 +0100

    Update TensileCompressiveStress.py

--- TensileCompressiveStress.py
+++ TensileCompressiveStress.py
@@ -1,5 +1,12 @@
 import numpy as np
 import scipy as sp
+
+
+# relevant dimensions 
+
+ts = 0.008 #m spar thickness
+tk = 0.005 #m web thickness
+
 # import inertia stuff and geomtry
 
 from wingbox_properties import *
@@ -8,8 +15,31 @@ from wingbox_properties import *
 
 from NormalShearMomentDiagrams import Momentforce, Momentforce1
 
-# calculate sigma function
+# calculate sigma functions for tension and compression
+def dz(y): #offset of vertical position of te spar
+    c = (dz_0-dz_1) / wingspan/2
+    dz = c*y+dz_0
+    return dz
 
+def sigma_ten(y):
+    if y < 0:
+        return 0
+    if y > wingspan/2:
+        return 0
+    z  = wb_centroid(y)[1]
+    sigma = Momentforce(y)*z / (I_xx(y, ts, tk) + I_xx_str(y, A_str))
+
+    return sigma
+
+
+def sigma_comp(y):
+    if y < 0:
+        return 0
+    if y > wingspan/2:
+        return 0
+    z = wb_centroid(y)[1] - (wb_rear_spar_h(y) + dz(y))
+    sigma = Momentforce(y)*z / (I_xx(y, ts, tk) + I_xx_str(y, A_str))
+    return sigma
 
 # for loop to make it into an array
 
